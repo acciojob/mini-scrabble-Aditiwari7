49179da33c83fe8f630c10eb058e476d7fab0635
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-18 18:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+
+const evaText = document.getElementById('evaluatedText');
+const count = document.getElementById('letterCount');
+
+evaText.addEventListener('input', () => {
+	const text = evaText.value;
+	count.textContent = text.length;
+})
